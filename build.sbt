688150763c9bfb1f78b4ffdfdc44c9114c1ec0ff
michellemay <mlemay@gmail.com>
2015-03-13 13:08:15 -0400

    Add some case classes and json formatting

--- build.sbt
+++ build.sbt
@@ -12,10 +12,11 @@ libraryDependencies ++= {
   Seq(
     "io.spray"            %%  "spray-can"     % sprayV,
     "io.spray"            %%  "spray-routing" % sprayV,
+    "io.spray"            %%  "spray-json"    % "1.3.1",
     "io.spray"            %%  "spray-testkit" % sprayV  % "test",
     "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
     "com.typesafe.akka"   %%  "akka-testkit"  % akkaV   % "test",
-    "org.specs2"          %%  "specs2-core"   % "2.3.7" % "test"
+    "org.specs2"          %%  "specs2-core"   % "2.3.11" % "test"
   )
 }
 
--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -4,6 +4,7 @@ import akka.actor.Actor
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json.{JsonFormat, DefaultJsonProtocol}
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -19,11 +20,31 @@ class MyServiceActor extends Actor with MyService {
   def receive = runRoute(myRoute)
 }
 
+case class Query(query:String)
+case class Prediction(document:String, score:Double)
+case class PredictionResult(predictions:List[Prediction])
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+    implicit val impQueryFormat = jsonFormat1(Query)
+    implicit val predictionFormat = jsonFormat2(Prediction)
+    implicit val predictionResultFormat = jsonFormat1(PredictionResult)
+}
 
 // this trait defines our service behavior independently from the service actor
-trait MyService extends HttpService {
-
-  val myRoute =
+trait MyService extends HttpService with spray.httpx.SprayJsonSupport {
+  val myRoute = {
+    import MyJsonProtocol._
+    path("predict") {
+      get {
+        parameters('q) { q => {
+            complete {
+              val result = PredictionResult(List(Prediction("a", 1.0), Prediction("b", 0.5)))
+              result
+            }
+          }
+        }
+      }
+    } ~
     path("") {
       get {
         respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
@@ -36,5 +57,6 @@ trait MyService extends HttpService {
           }
         }
       }
-    }
+    }    
+  }
 }
